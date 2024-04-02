1cd50374c6df867c4ec22d86677e3e0cea77b839
ananidev <mohammad.anani01@outlook.com>
2024-04-03 02:57:21 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const name = "Mohammad Anani";
+  const age = "23"
+  return (
+    <>
+      <div>
+        <h1>Hello {name}, your age is {age}</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
